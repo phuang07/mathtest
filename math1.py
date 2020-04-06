3bd950ee32d97dde43be4067a393d60b6b470be1
Ray Huang <leoman730@gmail.com>
2020-04-06 13:54:51 -0400

    Merge

--- math1.py
+++ math1.py
@@ -1,18 +1,20 @@
 import random
 import sys 
 import subprocess
+import timeit
 
-subprocess.check_call([sys.executable, "-m", "pip", "install", "colored"])
-subprocess.check_call([sys.executable, "-m", "pip", "install", "cprint"])
+# subprocess.check_call([sys.executable, "-m", "pip", "install", "colored"])
+# subprocess.check_call([sys.executable, "-m", "pip", "install", "cprint"])
 subprocess.check_call([sys.executable, "-m", "pip", "install", "termcolor"])
 
 from termcolor import colored, cprint
 
-num_question = 20 
+num_question = 2 
 min_num = 1
 max_num = 10
 num_correct=0
 
+start = timeit.default_timer()
 
 while(num_correct < num_question):
     num1 = random.randint(min_num, max_num)
@@ -24,7 +26,11 @@ while(num_correct < num_question):
         answer=input()
 
     cprint("\u2713 Correct!","green")
+    cprint(f"Progress: {num_correct}/{num_question}","green")
     
     num_correct = num_correct + 1
 
+end = timeit.default_timer()
+elaspe = format(end - start, '.2f')
+cprint(f"Total time: {elaspe}s")
 print("Good Bye")
